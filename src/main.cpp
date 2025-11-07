4b1362ed122b724189dde8799dac3cf8bcd0effd
John <yangtianzhuo970425@gmail.com>
2025-11-07 16:23:58 +1300

    Update .gitignore, .clang-format, .clang-tidy, and VS Code settings; refactor main logic to utilize new bit manipulation functions and improve code organization

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,26 @@
+#include <cstdio>
+
 #include "defines.hpp"
 
 int main() {
-  U64 primaryBoard = 0ULL;
-  SetBit(primaryBoard, 61);
-  SetBit(primaryBoard, 12);
+    int PieceOne = rand();
+    int PieceTwo = rand();
+    int PieceThree = rand();
+    int PieceFour = rand();
 
-  std::cout << "Initial board state:";
-  PrintBitboard(primaryBoard);
+    printf("Pieces: %X\n", PieceOne);
+    printf("Pieces: %X\n", PieceTwo);
+    printf("Pieces: %X\n", PieceThree);
+    printf("Pieces: %X\n", PieceFour);
 
-  std::cout << "Active squares: " << CountBits(primaryBoard) << "\n";
+    int Key = PieceOne ^ PieceTwo ^ PieceThree ^ PieceFour;
+    int TempKey = PieceOne;
+    TempKey ^= PieceThree;
+    TempKey ^= PieceFour;
+    TempKey ^= PieceTwo;
 
-  auto workingBoard = primaryBoard;
-  const auto poppedIndex = PopBit(workingBoard);
-  std::cout << "After popping least-significant bit (index " << poppedIndex
-            << "):";
-  PrintBitboard(workingBoard);
+    printf("Key: %X\n", Key);
+    printf("TempKey: %X\n", TempKey);
 
-  ClearBit(primaryBoard, 61);
-  std::cout << "After clearing square 61:";
-  PrintBitboard(primaryBoard);
-
-  return 0;
+    return 0;
 }
